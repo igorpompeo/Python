d13b3103e89d024e51edde5135bdb232853030ef
IgorPompeo <ipsouza@bbmapfre.com.br>
2025-04-25 15:00:37 -0300

    Ajustes no README

---
+++ menu.py
@@ -0,0 +1,14 @@
+# menu.py
+import os
+
+while True:
+    escolha = input("Digite o número do exercício (ex: 001), ou 'sair': ")
+    if escolha == "sair":
+        break
+    
+    caminho = os.path.join("Mundo01", "Exercicios", f"ex{escolha}.py")
+    
+    if os.path.exists(caminho):
+        exec(open(caminho, encoding="utf-8").read())
+    else:
+        print("Arquivo não encontrado.")
