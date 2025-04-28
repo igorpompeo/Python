188631b7eb5a3857aa45b12d7c69b334516c636d
IgorPompeo <ipsouza@bbmapfre.com.br>
2025-04-28 08:33:43 -0300

    Criação dos desafios/exercícios do 5 ao 13 e inclusão de um template para pull request

---
+++ Mundo01/Desafios/12/12.py
@@ -0,0 +1,9 @@
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+print('=== Desafio 12 ===')
+print('=== Desconto de 5% ===')
+preco = float(input('Digite o preço do produto: R$ '))
+desconto = preco * 0.05
+preco_final = preco - desconto
+print(f'O preço do produto com 5% de desconto é: R$ {preco_final:.2f}')
+print('=== Fim do Desafio 12 ===')
+# # Fim do desafio 12
