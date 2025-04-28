ea9795248ac0dea4e0ead0cab2386f85f3a2c388
IgorPompeo <ipsouza@bbmapfre.com.br>
2025-04-28 10:51:02 -0300

    Incluindo apenas customização

--- Mundo01/Exercicios/ex009.py
+++ Mundo01/Exercicios/ex009.py
@@ -5,10 +5,12 @@ print('Tabuada')
 
 # Gera um número aleatório de 1 a 100
 n = random.randint(1, 100)
+print('-' * 20)
 print(f'A tabuada de {n} é:')
 
 for i in range(1, 11):
-    print(f'{n} x {i} = {n * i}')
+    print(f'{n:2} x {i:2} = {n * i}')
 
+print('-' * 20)
 print('=== Fim do Desafio 09 ===')
 # Fim do desafio 09
