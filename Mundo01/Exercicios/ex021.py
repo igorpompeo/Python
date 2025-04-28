b0b976d38b7149e1b6de1a1cc6f2fd0a8bf43d76
IgorPompeo <ipsouza@bbmapfre.com.br>
2025-04-28 16:06:31 -0300

    Ajustes de caminho dos objetos do exercício

--- Mundo01/Exercicios/ex021.py
+++ Mundo01/Exercicios/ex021.py
@@ -5,19 +5,21 @@
 import pygame
 import os
 # Lendo o nome do arquivo de áudio do usuário
-print('Digite o nome do arquivo de áudio (com extensão .mp3): ')
+print('Digite o nome do arquivo de áudio (com extensão .mp3/.wav): ')
 # Gerando um nome de arquivo aleatório
-arquivo = 'musica.mp3'  # Nome do arquivo de áudio
+arquivo = os.path.join('C:\\Windows\\Media', 'Windows Shutdown.wav')  # Nome do arquivo de áudio
 # Exibindo o nome do arquivo
 print(f'Você digitou: {arquivo}')
 # Verificando se o arquivo existe
 if os.path.exists(arquivo):
+    pygame.init()  # Inicializando o pygame
     # Reproduzindo o arquivo de áudio
     print(f'Reproduzindo o arquivo de áudio: {arquivo}...')
     # Inicializando o mixer do pygame
     pygame.mixer.music.load(arquivo)
     # Colocando para tocar o arquivo de áudio
     pygame.mixer.music.play()
+    pygame.event.wait()  # Esperando o áudio terminar de tocar
 else:
     # Exibindo mensagem de erro se o arquivo não existir
     print(f'Erro: O arquivo {arquivo} não foi encontrado.')
